94c9df0aae9e4bfaba37ef8fb17299a58d9ef3f9
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-04-27 14:46:15 +0100

    answers for pseudocode excercises with just pseudocode

--- thinkingLogicallyForCoding/fundamentals-of-pseudocode excercises.js
+++ thinkingLogicallyForCoding/fundamentals-of-pseudocode excercises.js
@@ -1,19 +1,41 @@
+
+
+
+function randomNumberArray(min, max){
+    let newNumberArray = [];
+
+    for (let i = 0; i < 10; i++){
+        newNumberArray.push(Math.floor(Math.random() * (max - min) + min));
+    }
+
+    return newNumberArray;
+}
+
+
+
+
 // ---------------------------------------------------------------------------------
 //excercise 1 - find the largest number
 
+
 //PROGRAM <findLargestNumber>
 
+    //define variable arrayOfCards
     //define variable highestCard
+
     //forEach card in  arrayOfCards DO:
     //    IF (currentCard > highestCard) DO:
     //       highestCard = currentCard
+    //        move onto next card
     //    ELSE
     //        move onto next card
     //    END IF
     //END forEach
-    //THEN return highestCard
+    //RETURN highestCard
 
 //END
+
+
 
 
 // ---------------------------------------------------------------------------------
@@ -25,13 +47,17 @@
     //forEach card in the arrayOfCards DO:
     //    IF (currentCard < lowestCard) DO:
     //       lowestCard = currentCard
-    //    ELSE
+    //        move onto next card
+    //    ELSE  
     //        move onto next card
     //    END IF
     //END forEach
-    //THEN return lowestCard
+    //RETURN lowestCard
 
 //END
+
+
+
 
 // ---------------------------------------------------------------------------------
 //excercise 3 - is in list
@@ -48,7 +74,7 @@
     //        move onto next card
     //    END IF
     //END forEach
-    //THEN return answer
+    //RETURN answer
 
 //END
 
@@ -66,68 +92,238 @@
     //forEach currentNumber in numberList DO:
         //sumTotal = addTwoNumbers(sumTotal, currentNumber)
     //END forEach
+    
     //return sumTotal
     
+//END
+
+// ---------------------------------------------------------------------------------
+//excercise 5 - find the largest even number
+
+//PROGRAM <largestEvenNumber>
+
+    //define variable numberList
+    //define variable largestEvenNumber
+
+    //define FUNCTION  <compareNumbers> with arguments (currentLargest AND number) DO:
+        //IF (number > currentLargest) AND (number % 2 == 0) DO:
+            //RETURN TRUE
+        //ELSE DO:
+            //RETURN FALSE
+        //END IF
+
+        //forEach currentNumber in numberList DO:
+            //define variable isEvenAndLarger = function: compareNumbers with (currentLargest as largestEvenNumber) and (currentNumber as number)
+            //IF (isEvenAndLarger == TRUE) DO:
+                //largestNumber = currentNumber
+                //move onto next card
+            //ELSE DO:
+                //move onto next number
+            //END IF
+        //END forEach
+
+    //RETURN largestEvenNumber
+
+
+//END
+
+// ---------------------------------------------------------------------------------
+//excercise 6 - find the largest odd number
+
+//PROGRAM <largestOddNumber>
+
+    //define variable numberList
+    //define variable largestOddNumber
+
+    //define FUNCTION  <compareNumbers> with arguments (currentLargest AND number) DO:
+        //IF (number > currentLargest) AND (number % 2 =/= 0) DO:
+            //RETURN TRUE
+        //ELSE DO:
+            //RETURN FALSE
+        //END IF
+
+        //forEach currentNumber in numberList DO:
+            //define variable isOddAndLarger = function: compareNumbers with (currentLargest as largestOddNumber) and (currentNumber as number)
+            //IF (isOddAndLarger == TRUE) DO:
+                //largestNumber = currentNumber
+                //move onto next card
+            //ELSE DO:
+                //move onto next card
+            //END IF
+        //END forEach
+
+    //RETURN largestOddNumber
+
+
+//END
+
+// ---------------------------------------------------------------------------------
+//excercise 7 - find second largest number
+
+//PROGRAM <programName>
+
+//define variable numberList
+//define variable 
+
+
+
+
+//END
+
+// ---------------------------------------------------------------------------------
+//excercise 8 - count specific number
+
+//PROGRAM <programName>
+
+    //define variable numberList
+    //define variable count
+    //define variable target
+
+    //forEach (number in numberList) DO:
+        // IF (number = target) DO:
+            //count = count + 1
+            //move onto next card
+        //ELSE DO:
+            //move onto next number
+        //END IF
+
+    //RETURN count
+
+//END
+
+// ---------------------------------------------------------------------------------
+//excercise 9 - count positive numbers
+
+//PROGRAM <countPositiveNumbers>
+
+    //define variable numberList
+    //define variable count
+
+    //forEach number in numberList DO:
+        //IF (number > 0) DO:
+            //count = count + 1
+            //move onto next card
+        //ELSE DO:
+            //move onto next number
+        //END IF
+    //END forEach
+
+    //RETURN count
+
+
+//END
+
+// ---------------------------------------------------------------------------------
+//excercise 10 - count negative numbers
+
+//PROGRAM <countNegativeNumbers>
+
+    //define variable numberList
+    //define variable count
+
+    //forEach number in numberList DO:
+        //IF (number < 0) DO:
+            //count = count + 1
+            //move onto next card
+        //ELSE DO:
+            //move onto next number
+        //END IF
+    //END forEach
+
+    //RETURN count
+
+
 //END
 
 
 
 
-// ---------------------------------------------------------------------------------
-//excercise 5 - find the largest even number
-
-
-
-
-// ---------------------------------------------------------------------------------
-//excercise 6 - find the largest odd number
-
-
-
-
-// ---------------------------------------------------------------------------------
-//excercise 7 - find second largest number
-
-
-
-
-// ---------------------------------------------------------------------------------
-//excercise 8 - count specific number
-
-
-
-
-// ---------------------------------------------------------------------------------
-//excercise 9 - count positive numbers
-
-
-
-
-// ---------------------------------------------------------------------------------
-//excercise 10 - count negative numbers
-
-
-
+//END
 
 // ---------------------------------------------------------------------------------
 //excercise 11 - count larger than 10
 
+//PROGRAM <largerThan10>
+
+    //DEFINE VARIABLE numberList
+    //DEFINE VARIABLE count
+
+    //forEach currentNumber in numberList DO:
+        //IF (currentNumber > 10) DO:
+            //count = count + 1
+            //move onto next card
+        //ELSE
+            //move onto next number
+        //END IF
+
+        //RETURN count
 
 
+//END
 
 // ---------------------------------------------------------------------------------
 //excercise 12 - count smaller than 10
 
+//PROGRAM <smallerThan10>
 
+    //DEFINE VARIABLE numberList
+    //DEFINE VARIABLE count
+
+    //forEach currentNumber in numberList DO:
+        //IF (currentNumber > 10) DO:
+            //count = count + 1
+            //move onto next card
+        //ELSE
+            //move onto next number
+        //END IF
+
+    //RETURN count
+
+
+//END
 
 
 // ---------------------------------------------------------------------------------
 //excercise 13 - count larger than x
 
+//PROGRAM <largerThan10>
 
+    //DEFINE VARIABLE numberList
+    //DEFINE VARIABLE count
+    //DEFINE VARIABLE target
+
+    //forEach currentNumber in numberList DO:
+        //IF (currentNumber > target) DO:
+            //count = count + 1
+            //move onto next card
+        //ELSE
+            //move onto next number
+        //END IF
+
+    //RETURN count
+
+
+//END
 
 
 // ---------------------------------------------------------------------------------
 //excercise 14 - count smaller than x
 
+//PROGRAM <smallerThan10>
 
+    //DEFINE VARIABLE numberList
+    //DEFINE VARIABLE count
+    //DEFINE VARIABLE target
+
+    //forEach currentNumber in numberList DO:
+        //IF (currentNumber > target) DO:
+            //count = count + 1
+            //move onto next card
+        //ELSE
+            //move onto next number
+        //END IF
+
+    //RETURN count
+
+
+//END
