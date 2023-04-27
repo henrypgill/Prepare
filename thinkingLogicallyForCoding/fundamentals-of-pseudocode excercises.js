53d90d6b83297f630a26bef4964644e69ca6197c
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-04-27 13:09:00 +0100

    initial commit, with first answers to the fundamentals of pseudocode excercises

---
+++ thinkingLogicallyForCoding/fundamentals-of-pseudocode excercises.js
@@ -0,0 +1,133 @@
+// ---------------------------------------------------------------------------------
+//excercise 1 - find the largest number
+
+//PROGRAM <findLargestNumber>
+
+    //define variable highestCard
+    //forEach card in  arrayOfCards DO:
+    //    IF (currentCard > highestCard) DO:
+    //       highestCard = currentCard
+    //    ELSE
+    //        move onto next card
+    //    END IF
+    //END forEach
+    //THEN return highestCard
+
+//END
+
+
+// ---------------------------------------------------------------------------------
+//excercise 2 - find the smallest number
+
+//PROGRAM <findSmallestNumber>
+
+    //define variable lowestCard
+    //forEach card in the arrayOfCards DO:
+    //    IF (currentCard < lowestCard) DO:
+    //       lowestCard = currentCard
+    //    ELSE
+    //        move onto next card
+    //    END IF
+    //END forEach
+    //THEN return lowestCard
+
+//END
+
+// ---------------------------------------------------------------------------------
+//excercise 3 - is in list
+
+//PROGRAM <isInList>
+
+    //define variable targetNumber
+    //define variable answer = FALSE
+    //forEach card in the arrayOfCards DO:
+    //    IF currentCard = targetNumber DO:
+    //       answer = TRUE
+    //       END forEach
+    //    ELSE
+    //        move onto next card
+    //    END IF
+    //END forEach
+    //THEN return answer
+
+//END
+
+// ---------------------------------------------------------------------------------
+//excercise 4 - add numbers
+
+//PROGRAM <numberListSum>
+    //define variable sumTotal = 0
+    //define list numberList
+
+    //FUNCTION <addTwoNumbers> (number1, number2)
+    //RETURN number1 + number2
+    //END FUNCTION
+
+    //forEach currentNumber in numberList DO:
+        //sumTotal = addTwoNumbers(sumTotal, currentNumber)
+    //END forEach
+    //return sumTotal
+    
+//END
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 5 - find the largest even number
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 6 - find the largest odd number
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 7 - find second largest number
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 8 - count specific number
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 9 - count positive numbers
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 10 - count negative numbers
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 11 - count larger than 10
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 12 - count smaller than 10
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 13 - count larger than x
+
+
+
+
+// ---------------------------------------------------------------------------------
+//excercise 14 - count smaller than x
+
+
